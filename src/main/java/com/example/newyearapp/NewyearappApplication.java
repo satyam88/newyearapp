55295ea13ca212f9c8e3faa4a735ef59c81414e7
satyam88 <nokia123>
2019-12-29 18:13:11 +0530

    codeRefactor

---
+++ src/main/java/com/example/newyearapp/NewyearappApplication.java
@@ -0,0 +1,13 @@
+package com.example.newyearapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewyearappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewyearappApplication.class, args);
+	}
+
+}
